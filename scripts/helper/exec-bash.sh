8dcf0803e94f9d1a87056a26796d4c3f0ebecd4b
Troy Dai <1329240+troydai@users.noreply.github.com>
2023-04-23 09:53:11 -0700

    Improve bash execution script (#6)

--- scripts/helper/exec-bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NS=$1
-echo "Exec bash on pod in namespace $NS"
-
-POD=`kubectl get pod -n $NS -ojson | jq -r '.items[0].metadata.name'`
-kubectl -it exec -n $NS -c toolbox $POD -- bash
