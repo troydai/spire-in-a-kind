ee5b251be142546587628183daa7e004fb065fb6
Troy Dai <git@troydai.com>
2023-04-27 21:39:45 -0700

    rename elements

--- scripts/entry/clean.sh
+++ scripts/entry/clean.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-for name in red blue; do
-    ns=httpbin-$name
-    helm list -n $ns -ojson | jq -r '.[0] | .name' | xargs helm uninstall -n $ns
-done
-
-for name in alice bob charlie; do
-    ns=caller-$name
-    helm list -n $ns -ojson | jq -r '.[0] | .name' | xargs helm uninstall -n $ns
-done
+TRUST_DOMAIN=spire-in-a-box.troydai.cc
+CLUSTER=kind-cluster
 
 EXISTING_ENTRIES=`kubectl exec -n spire -c spire-server deployment/spire-server-deployment -- /opt/spire/bin/spire-server entry show -output json | jq -r '.entries[].id'`
 
-echo "Deleting existing SPIRE entries..."
+echo "Deleting existing entries..."
 
 for entry in $EXISTING_ENTRIES; do
     kubectl exec -n spire -c spire-server deployment/spire-server-deployment -- /opt/spire/bin/spire-server entry delete -entryID $entry
--- scripts/setup/create-workloads.sh
+++ scripts/setup/create-workloads.sh
@@ -16,25 +16,25 @@ kubectl exec -n spire -c spire-server deployment/spire-server-deployment -- /opt
     -selector k8s_psat:cluster:$CLUSTER
 
 for name in red blue; do
-    ns=beacon-$name
-    helm install -n $ns --create-namespace -g charts/beacon --set beacon.name=$name --values workloads/beacon/$name/values.yaml
+    ns=httpbin-$name
+    helm install -n $ns --create-namespace -g charts/httpbin --values workloads/httpbin/$name/values.yaml
 
     kubectl exec -n spire -c spire-server deployment/spire-server-deployment -- /opt/spire/bin/spire-server \
         entry create \
-        -spiffeID spiffe://$TRUST_DOMAIN/wl/ns/$ns/beacon \
+        -spiffeID spiffe://$TRUST_DOMAIN/wl/ns/$ns/httpbin \
         -parentID spiffe://$TRUST_DOMAIN/ns/spire/sa/spire-agent \
         -selector k8s:ns:$ns \
-        -selector k8s:sa:beacon-sa
+        -selector k8s:sa:httpbin-sa
 done
 
 for name in alice bob charlie; do
-    ns=prober-$name
-	helm install -n $ns --create-namespace -g charts/prober --values workloads/prober/$name/values.yaml
+    ns=caller-$name
+	helm install -n $ns --create-namespace -g charts/caller
 
     kubectl exec -n spire -c spire-server deployment/spire-server-deployment -- /opt/spire/bin/spire-server \
         entry create \
-        -spiffeID spiffe://$TRUST_DOMAIN/wl/ns/$ns/prober \
+        -spiffeID spiffe://$TRUST_DOMAIN/wl/ns/$ns/toolbox \
         -parentID spiffe://$TRUST_DOMAIN/ns/spire/sa/spire-agent \
         -selector k8s:ns:$ns \
-        -selector k8s:sa:prober-sa
+        -selector k8s:sa:caller-sa
 done
